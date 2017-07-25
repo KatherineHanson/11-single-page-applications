32c0ac35558ef86c03a36db340318b8eff52ed1c
Chaitanya.Narukulla <Chai@Ashleys-MacBook-Air.local>
2017-07-25 09:32:47 -0700

    REfactored TODOs

--- starter-code/public/scripts/controllers/articleController.js
+++ starter-code/public/scripts/controllers/articleController.js
@@ -5,7 +5,7 @@
   // DONE: Setup a function that kicks off the fetching and rendering of articles, using the same
   // code that used to be in index.html.
   // Also be sure to hide all the main section elements, and reveal the #articles section:
-  app.articleView.articleController = function() {
+  articleController.init = function() {
     $('.main-nav').on('click', '.tab', function() {
       $('.tab-content').hide();
       $(`#${$(this).data('content')}`).fadeIn();
